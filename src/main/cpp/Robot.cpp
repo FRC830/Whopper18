ba73cab47760d1ef29d0626e766ab7267ebf7b3a
Joey Bishop <JoooooooeyBishop@FRC.830>
2018-11-18 15:11:43 -0500

    Added gyro correct

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,7 +1,5 @@
 
 #include "Robot.h"
-#include <Lib830.h>
-// Including Lib830 using full path within build.gradle/wpilib_preferences.json
 #include <iostream>
 
 
@@ -23,8 +21,10 @@ static const GenericHID::JoystickHand RIGHT = GenericHID::kRightHand;
 
 XboxController pilot{0};
 AnalogGyro gyro{GYRO_ANALOG_IN};
+Timer autonTimer{};
 
 void Robot::RobotInit() {
+    gyro.Calibrate();
 
 }
 
@@ -33,25 +33,43 @@ void Robot::RobotPeriodic() {
 }
 
 void Robot::AutonomousInit() {
+    // Reset Timer
+    autonTimer.Reset();
+    autonTimer.Start();
 }
-
 void Robot::AutonomousPeriodic() {
+    if (autonTimer.Get() < 8) {
+        //m_drive.DriveCartesian(0.2, 0, 0);
+        double gyroReading = gyro.GetAngle();
+        SmartDashboard::PutNumber("getAngleAuto",gyroReading);
+        double correctedGyro = - gyroReading / 90;
+        m_drive.DriveCartesian(0.4, 0, correctedGyro);
+    } else {
+        autonTimer.Stop();
+    }
+    
+    
 }
 
 void Robot::TeleopInit() {
-    // supposedly 'calibrates' the robot.
+    // supposedly 'calibrates' the robot, doesn't actually.
     gyro.Calibrate();
 
 }
-
+double SmoothCosineTransformation(double input) {
+    // Use a cosine function to activate a smooth start and stop
+    double output = input;
+    return output;
+}
 void Robot::TeleopPeriodic() {
     // using a ternary operator to set rawX/Y to 0 if value is below minimum threshold
-    double rawX = abs(pilot.GetX(LEFT)) < DEADZONE_THRESHOLD ? 0 : pilot.GetX(LEFT)
-    double rawY = abs(pilot.GetY(LEFT)) < DEADZONE_THRESHOLD ? 0 : pilot.GetY(LEFT)
+    double rawX = abs(pilot.GetX(LEFT)) < DEADZONE_THRESHOLD ? 0 : pilot.GetX(LEFT);
+    double rawY = abs(pilot.GetY(LEFT)) < DEADZONE_THRESHOLD ? 0 : pilot.GetY(LEFT);
     double turn = pilot.GetX(RIGHT);
 
     // Debug
     SmartDashboard::PutNumber("getAngle",gyro.GetAngle());
+
     SmartDashboard::PutData("Gyro",&gyro);
 
     double y = SmoothCosineTransformation(-rawY); // Testing, robot has inverse controls
@@ -59,11 +77,7 @@ void Robot::TeleopPeriodic() {
     m_drive.DriveCartesian(y , x, turn);
 
 }
-SmoothCosineTransformation(input double) {
-    // Use a cosine function to activate a smooth start and stop
-    double output = input;
-    return output
-}
+
 void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
