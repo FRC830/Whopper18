74443f91f8aeb2fecea28cb4b2c6972a8ac67e59
Dean Martin <deanmartin@830>
2018-11-18 13:05:49 -0500

    Deadzone, Pins are Lincoln, and gyro correct

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,16 +1,17 @@
 
 #include "Robot.h"
-
+#include <Lib830.h>
+// Including Lib830 using full path within build.gradle/wpilib_preferences.json
 #include <iostream>
 
 
-#include <SmartDashboard/SmartDashboard.h>
 
-static  const int FRONT_LEFT_WHEEL = 1;  //Random Values
-static const int FRONT_RIGHT_WHEEL = 2;
-static const int BOTTOM_LEFT_WHEEL = 3;
-static const int BOTTOM_RIGHT_WHEEL = 4;
-
+static  const int FRONT_LEFT_WHEEL = 0;  //Same values as Robot2018 Github
+static const int FRONT_RIGHT_WHEEL = 6;
+static const int BOTTOM_LEFT_WHEEL = 1;
+static const int BOTTOM_RIGHT_WHEEL = 7;
+static const int GYRO_ANALOG_IN = 0;
+static const double DEADZONE_THRESHOLD = .05;
 VictorSP frontl{FRONT_LEFT_WHEEL};
 VictorSP frontr{FRONT_RIGHT_WHEEL};
 VictorSP backl{BOTTOM_LEFT_WHEEL};
@@ -21,6 +22,7 @@ static const GenericHID::JoystickHand LEFT = GenericHID::kLeftHand;
 static const GenericHID::JoystickHand RIGHT = GenericHID::kRightHand;
 
 XboxController pilot{0};
+AnalogGyro gyro{GYRO_ANALOG_IN};
 
 void Robot::RobotInit() {
 
@@ -37,18 +39,31 @@ void Robot::AutonomousPeriodic() {
 }
 
 void Robot::TeleopInit() {
+    // supposedly 'calibrates' the robot.
+    gyro.Calibrate();
 
 }
 
 void Robot::TeleopPeriodic() {
-    double x = pilot.GetX(LEFT);
-    double y = pilot.GetY(LEFT);
+    // using a ternary operator to set rawX/Y to 0 if value is below minimum threshold
+    double rawX = abs(pilot.GetX(LEFT)) < DEADZONE_THRESHOLD ? 0 : pilot.GetX(LEFT)
+    double rawY = abs(pilot.GetY(LEFT)) < DEADZONE_THRESHOLD ? 0 : pilot.GetY(LEFT)
     double turn = pilot.GetX(RIGHT);
 
-    m_drive.DriveCartesian(x , y, turn);
+    // Debug
+    SmartDashboard::PutNumber("getAngle",gyro.GetAngle());
+    SmartDashboard::PutData("Gyro",&gyro);
+
+    double y = SmoothCosineTransformation(-rawY); // Testing, robot has inverse controls
+    double x = SmoothCosineTransformation(rawX);
+    m_drive.DriveCartesian(y , x, turn);
 
 }
-
+SmoothCosineTransformation(input double) {
+    // Use a cosine function to activate a smooth start and stop
+    double output = input;
+    return output
+}
 void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
