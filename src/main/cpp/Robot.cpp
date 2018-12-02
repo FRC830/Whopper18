720e3223599e5953a0237f23cc7058dfa06a8d5a
Joey Bishop <JoooooooeyBishop@FRC.830>
2018-12-02 13:46:39 -0500

    added ultrasonic sensor

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -10,6 +10,10 @@ static const int BOTTOM_LEFT_WHEEL = 1;
 static const int BOTTOM_RIGHT_WHEEL = 7;
 static const int GYRO_ANALOG_IN = 0;
 static const double DEADZONE_THRESHOLD = .05;
+static const int ULTRASONIC_PING_PIN = 8;
+static const int ULTRASONIC_ECHO_PIN = 9;
+Ultrasonic sonic_sensor{ULTRASONIC_PING_PIN, ULTRASONIC_ECHO_PIN};
+
 VictorSP frontl{FRONT_LEFT_WHEEL};
 VictorSP frontr{FRONT_RIGHT_WHEEL};
 VictorSP backl{BOTTOM_LEFT_WHEEL};
@@ -27,7 +31,8 @@ static const int TICKS_TO_ACCEL = 20;
 
 void Robot::RobotInit() {
     gyro.Calibrate();
-
+    sonic_sensor.SetEnabled(true);
+    sonic_sensor.SetAutomaticMode(true);
 }
 
 void Robot::RobotPeriodic() {
@@ -38,16 +43,22 @@ void Robot::AutonomousInit() {
     // Reset Timer
     autonTimer.Reset();
     autonTimer.Start();
+
 }
 void Robot::AutonomousPeriodic() {
-    if (autonTimer.Get() < 8) {
+    double rangeMeasurement = sonic_sensor.GetRangeInches();
+    SmartDashboard::PutNumber("sonicRange (in)", rangeMeasurement);
+    int distance_from_center = 70;
+    int random_threshold = 5;
+    if (rangeMeasurement <= random_threshold || rangeMeasurement >= distance_from_center) {
         //m_drive.DriveCartesian(0.2, 0, 0);
         double gyroReading = gyro.GetAngle();
         SmartDashboard::PutNumber("getAngleAuto",gyroReading);
         double correctedGyro = - gyroReading / 90;
         m_drive.DriveCartesian(0.4, 0, correctedGyro);
     } else {
-        autonTimer.Stop();
+        m_drive.DriveCartesian(0, 0, 0);
+
     }
     
     
@@ -55,7 +66,6 @@ void Robot::AutonomousPeriodic() {
 
 void Robot::TeleopInit() {
     // supposedly 'calibrates' the robot, doesn't actually.
-    gyro.Calibrate();
 
 }
 
@@ -76,6 +86,7 @@ void Robot::TeleopPeriodic() {
     double turn = pilot.GetX(RIGHT);
 
     // Debug
+
     SmartDashboard::PutNumber("getAngle",gyro.GetAngle());
 
     SmartDashboard::PutData("Gyro",&gyro);
